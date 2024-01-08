98a114004e6010808ae40d56608a504607857dab
Stefan Hübner <stefan.huebner97@web.de>
2024-01-08 12:53:51 +0100

    projectspage

--- centralDatabase/database/seeders/ProjectSeeder.php
+++ centralDatabase/database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Project::create([
+            'user_id' => 1,
+            'name' => 'Test',
+        ]);
     }
 }
--- centralDatabase/resources/js/pages/Projects.vue
+++ centralDatabase/resources/js/pages/Projects.vue
@@ -1,34 +1,35 @@
 <script setup>
-import { useRoute, useRouter } from 'vue-router';
-import { onMounted, ref, computed } from 'vue';
-import { usehandledataManager } from '@/composables/handledataManager.js';
+import { useRoute, useRouter } from "vue-router";
+import { onMounted, ref, computed } from "vue";
+import { usehandledataManager } from "@/composables/handledataManager.js";
 
-const dataManager = usehandledataManager(); 
+const dataManager = usehandledataManager();
 
 // For routing ---
 const router = useRouter();
 const route = useRoute();
 
 // Input Overlay ---
-const projectname = ref('');
+const mynewprojectname = ref("");
 
 // Input Overlay ---
 const projects = ref([]);
+const newprojectbox = ref([]);
 const overlayRef = ref(null);
 const formData = ref({});
 const uploadPath = ref(null);
 
 async function fetchData() {
     try {
-        const result = await dataManager.getData('/api/getUserProjects');
+        const result = await dataManager.getData("/api/getUserProjects");
         if (result) {
             projects.value = result.data;
-            console.log('Datei wurde ausgewählt:', projects);
+            console.log("Datei wurde ausgewählt:", projects);
         } else {
-            console.error('Fehler beim Abrufen der Daten');
+            console.error("Fehler beim Abrufen der Daten");
         }
     } catch (error) {
-        console.error('Fehler beim Abrufen der Daten:', error);
+        console.error("Fehler beim Abrufen der Daten:", error);
     }
 }
 
@@ -38,35 +39,45 @@ onMounted(() => {
 
 const addNewProject = () => {
     if (overlayRef.value) {
-        overlayRef.value.classList.remove('d-none');
-        // Oder um es zu verstecken: 
+        overlayRef.value.classList.remove("d-none");
+        // Oder um es zu verstecken:
     } else {
-        console.error('Overlay-Element nicht gefunden.');
+        console.error("Overlay-Element nicht gefunden.");
     }
 };
 
 function submitNewProject() {
+    debugger;
     const newprojectdata = new FormData();
-    newprojectdata.append('projectname', projectname);
+    newprojectdata.append("projectname", mynewprojectname.value);
+    console.log(newprojectdata);
+    console.log(newprojectdata.projectname);
 
-    dataManager.postData('/api/createNewUserProject', newprojectdata)
+    dataManager
+        .postData("/api/createNewUserProject", newprojectdata)
         .then((responseData) => {
             //console.log('Antwort vom Server:', responseData);
-            if (responseData && responseData.uploaded_path) {
-                uploadPath.value = responseData.uploaded_path;
+            if (responseData) {
+                newprojectbox.value = responseData.name;
+                fetchData();
             } else {
-                console.error('Ungültige Serverantwort');
+                console.error("Ungültige Serverantwort");
             }
+            // if (responseData && responseData.uploaded_path) {
+            //     uploadPath.value = responseData.uploaded_path;
+            // } else {
+            //     console.error("Ungültige Serverantwort");
+            // }
         })
         .catch((error) => {
-            console.error('Fehler beim Hochladen der Datei:', error);
+            console.error("Fehler beim Hochladen der Datei:", error);
         });
 
-    overlayRef.value.classList.add('d-none');
+    overlayRef.value.classList.add("d-none");
 }
 
 function deleteProject(projectID) {
-    console.log('Z72', projectID);
+    console.log("Z72", projectID);
     // dataManager.deleteData(fileData)
     //     .then((responseData) => {
     //         //console.log('Antwort vom Server:', responseData);
@@ -83,8 +94,11 @@ function deleteProject(projectID) {
 
 function selectProject(project) {
     const projectname = project.name;
-    const routeData = router.resolve({ name: 'Board', params: { projectname } });
-    window.open(routeData.href, '_blank');
+    const routeData = router.resolve({
+        name: "Board",
+        params: { projectname },
+    });
+    window.open(routeData.href, "_blank");
 }
 
 // const onChangeLetters = (evt) => {
@@ -93,19 +107,19 @@ function selectProject(project) {
 //     //console.log(formData.value.file.name);
 
 //     // @change="onChangeLetters"
-       // NICHT DEN RICHTIGEN WERT ERHALTEN AUS DEM INPUT
+// NICHT DEN RICHTIGEN WERT ERHALTEN AUS DEM INPUT
 // };
-
 </script>
 
 <template>
     <div class="overlay center d-none" id="overlay" ref="overlayRef">
         <div class="overlay-content center">
-            <!-- <img src=""> -->
             <p>Neues Projekt</p>
-            <input name="mynewprojectname" id="mynewprojectname" type="text" ref="mynewprojectname" placeholder="Projekt Name">
+            <input name="mynewprojectname" type="text" ref="mynewprojectname" placeholder="Projekt Name">
 
-            <button @click.prevent="submitNewProject" class="primary-button">Hinzufügen</button>
+            <button @click.prevent="submitNewProject" class="primary-button">
+                Hinzufügen
+            </button>
         </div>
     </div>
 
@@ -113,12 +127,13 @@ function selectProject(project) {
         <customheader></customheader>
         <div class="wrapper center">
             <div class="projects">
-
                 <div class="create-card center">
                     <p>Create a new Project</p>
 
                     <div>
-                        <button class="primary-button" @click="addNewProject">CREATE</button>
+                        <button class="primary-button" @click="addNewProject">
+                            CREATE
+                        </button>
                         <button class="primary-button">SHARE</button>
                     </div>
                 </div>
@@ -129,19 +144,27 @@ function selectProject(project) {
                     </div>
 
                     <div class="card-actions">
-                        <button class="primary-button" @click="deleteProject(project.id)">Delete</button>
-                        <button class="primary-button" @click="selectProject(project)">Select</button>
+                        <button
+                            class="primary-button"
+                            @click="deleteProject(project.id)"
+                        >
+                            Delete
+                        </button>
+                        <button
+                            class="primary-button"
+                            @click="selectProject(project)"
+                        >
+                            Select
+                        </button>
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
-  
-<script>
-</script>
-  
+
+<script></script>
+
 <style>
-@import '@/sass/pages/projects.sass';
+@import "@/sass/pages/projects.sass";
 </style>
